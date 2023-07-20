3182196a5bbc4fb45e8eef115977826b5f236b12
Maharana Ashit <ashit.maharana@covalansedigital.com>
2023-07-20 10:59:46 +0530

    i have updated it for more fun

--- src/CrazyPrograms.java
+++ src/CrazyPrograms.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class CrazyPrograms {
+class crazy {
+    public static void show() {
+        System.out.println("welcome");
+    }
+}
+
+public class CrazyPrograms {
+    public static void main(String[] args) {
+        crazy.show();
+
+        // System.out.println("Hii Subrat");
+
+        /*int j=0;
+        int k=0;
+        for(int i=0;i<100;i++) {
+            j = j--;
+            k = ++k;
+        }
+        System.out.println(j+k);
+        if(j+k==100){
+            System.out.println("Done");
+        }
+        else
+            System.out.println("Not Done");*/
+
+
+    }
 }
---
+++ src/Date_Class.java
@@ -0,0 +1,11 @@
+
+
+public class Date_Class {
+    public static void main(String[] args) {
+        java.util.Date d1 = new java.util.Date();
+        System.out.println("Date is: " + d1);
+        long a = d1.getTime();
+        java.sql.Date sd1 = new java.sql.Date(a);
+        System.out.println("SQL Date Is: " + sd1);
+    }
+}
